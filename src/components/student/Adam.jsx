54b596f84d4959811a0e6db09a64e07f041494da
hammadqureshi31 <154003051+hammadqureshi31@users.noreply.github.com>
2025-02-24 22:21:32 +0500

    fix changes in Adam.jsx

--- src/components/student/Adam.jsx
+++ src/components/student/Adam.jsx
@@ -9,7 +9,7 @@ const Adam = () => {
   return (
     <div className="relative mt-7 md:mt-0 w-full bg-white py-28 px-6 
     sm:px-12 md:px-8 flex flex-col md:flex-row items-center justify-between
-     max-w-8xl mx-auto lg:px-14 xl:px-28">
+     max-w-8xl mx-auto lg:px-14 xl:px-16">
       {/* Decorative Background Elements */}
       <div className="absolute inset-0 overflow-hidden pointer-events-none">
         <Image src={cup} alt="Cup" width={100} height={100} className="absolute top-5 left-14 opacity-50" />
@@ -28,14 +28,16 @@ const Adam = () => {
             alt="Adam"
             // width={140}
             // height={140}
-            className="object-cover w-[140px] h-[140px] md:w-36 md:h-36 rounded-full"
+            className="object-cover w-[140px] h-[140px] md:w-36 md:h-36
+             rounded-full lg:w-44 lg:h-44"
           />
         </div>
         
         {/* Text Content */}
         <div className='md:w-1/2'>
-          <h2 className="text-3xl font-bold bg-gradient-to-r from-[#447EF7] to-[#243DBC] text-transparent bg-clip-text">Adam</h2>
-          <p className="text-gray-700 text-sm md:text-base mt-2 md:mt-0">
+          <h2 className="text-3xl font-bold bg-gradient-to-r from-[#447EF7]
+           to-[#243DBC] text-transparent bg-clip-text md:text-4xl lg:text-5xl">Adam</h2>
+          <p className="text-gray-700 text-sm md:text-base mt-2 md:mt-2 lg:text-lg">
             Becoming a Brand Ambassador gave me unique leadership experience and fulfilled my desire 
             to help other students succeed and reach their goals.
           </p>
@@ -43,10 +45,11 @@ const Adam = () => {
       </div>
       
       {/* Right Section */}
-      <div className="w-full  md:w-2/5 lg:w-2/4 flex flex-col items-start 
+      <div className="w-full  md:w-2/5 lg:w-2/4 flex  lg:mb-4  flex-col items-start 
       text-left sm:text-center sm:items-center mt-10 md:mt-0 md:items-start md:text-left">
-        <h3 className="text-2xl font-bold bg-gradient-to-r from-[#447EF7] to-[#243DBC] text-transparent bg-clip-text">Join Your Squad Today!</h3>
-        <p className="text-gray-700 text-sm md:text-base mt-2 md:mt-0">
+        <h3 className="text-2xl font-bold bg-gradient-to-r from-[#447EF7] to-[#243DBC] 
+        text-transparent bg-clip-text lg:text-5xl lg:text-nowrap ">Join Your Squad Today!</h3>
+        <p className="text-gray-700 text-sm md:text-base mt-2 md:mt-3 lg:text-lg">
           Become a Brand Ambassador and Start Making a Difference for Yourself and Your Peers.
         </p>
         <a
