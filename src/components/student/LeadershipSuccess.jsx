0f0e27da6dae99af25f190ae0d5e1ba752341098
hammadqureshi31 <154003051+hammadqureshi31@users.noreply.github.com>
2025-02-24 22:19:57 +0500

    fix changes in leadership section

--- src/components/student/LeadershipSuccess.jsx
+++ src/components/student/LeadershipSuccess.jsx
@@ -6,8 +6,8 @@ const LeadershipSuccess = () => {
     <>
       <div>
       <div className="flex flex-col xl:flex-row">
-        <div className="w-full xl:w-[900px] flex flex-col items-center xl:items-end justify-center xl:-mr-10 z-10 px-4 sm:px-6 md:px-8 lg:px-12 xl:px-0 py-12 xl:py-0">
-          <div className="max-w-[715px]">
+        <div className="w-full xl:w-[1100px] flex flex-col items-center  justify-center xl:-mr-10 z-10 px-4 sm:px-6 md:px-8 lg:px-12 xl:px-0 py-12">
+          <div className="max-w-[730px] lg:pb-5">
             <div className="text-4xl sm:text-5xl lg:text-[58px] font-bold text-left text-[#5277FF] leading-tight pb-6 sm:pb-8 lg:pb-10">
               Real LeaderShip. <br />
               Real Success!
@@ -16,7 +16,7 @@ const LeadershipSuccess = () => {
               <span className="font-bold">
                 Admissions officers love to see students who lead successful and
                 difficult projects.
-              </span>
+              </span> {" "}
               Dare to do what most teens won’t even dream of, and get
               tangible proof that you’re the kind of leader top universities want.
             </div>
@@ -24,24 +24,24 @@ const LeadershipSuccess = () => {
               <div className="flex space-x-4 items-start">
                 <Image
                   src="/tick_icon.svg"
-                  width={25}
-                  height={25}
+                  width={19}
+                  height={19}
                   alt="Tick Icon"
                   className="mt-1"
                 />
-                <div className="text-[#5D5D75] font-normal text-base sm:text-lg lg:text-[22px]">
+                <div className="text-[#5D5D75] font-normal text-base sm:text-lg lg:text-[18px]">
                   Other students will show potential. Ambassadors show results.
                 </div>
               </div>
               <div className="flex space-x-4 items-start mt-4">
                 <Image
                   src="/tick_icon.svg"
-                  width={25}
-                  height={25}
+                  width={19}
+                  height={19}
                   alt="Tick Icon"
                   className="mt-1"
                 />
-                <div className="text-[#5D5D75] font-normal text-base sm:text-lg lg:text-[22px]">
+                <div className="text-[#5D5D75] font-normal text-base sm:text-lg lg:text-[18px]">
                   As an Ambassador, you’ll know exactly what to write about in your
                   essays
                 </div>
@@ -49,12 +49,12 @@ const LeadershipSuccess = () => {
               <div className="flex space-x-4 items-start mt-4">
                 <Image
                   src="/tick_icon.svg"
-                  width={25}
-                  height={25}
+                  width={19}
+                  height={19}
                   alt="Tick Icon"
                   className="mt-1"
                 />
-                <div className="text-[#5D5D75] font-normal text-base sm:text-lg lg:text-[22px]">
+                <div className="text-[#5D5D75] font-normal text-base sm:text-lg lg:text-[18px]">
                   Ambassadors get skills and experience that most people don’t get
                   until they are out of college.
                 </div>
@@ -68,7 +68,7 @@ const LeadershipSuccess = () => {
             width={800}
             height={800}
             alt="Picture of the boy"
-            className="z-0 max-w-[90%]"
+            className="z-0 max-w-[90%] lg:min-h-full"
             />
             </div>
       </div>
