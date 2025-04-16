9fa2c04f37c9593a59fa45bc454b1d40b70a59e9
College Mastermind <developer@collegemastermind.com>
2025-04-16 11:50:20 -0400

    Update HeroSection.jsx

--- src/components/UniversityAdmissionPage/student/HeroSection.jsx
+++ src/components/UniversityAdmissionPage/student/HeroSection.jsx
@@ -19,7 +19,7 @@ export default function HeroSection() {
         </p>
 
         <a
-          href="https://collegemastermind-ambassador.pushlapgrowth.com/"
+          href="https://forms.gle/vpHcKmVJTaTWL8wVA"
           target="_blank"
           className="bg-white text-blue-500 font-semibold border text-sm
            border-blue-500 rounded-lg transition duration-300 ease-in-out 
